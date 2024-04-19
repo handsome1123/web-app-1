216da80b05a8fa4e9087e4fa66246fdff8147828
sannyuntwin <118452033+sawsannyunt@users.noreply.github.com>
2024-04-20 01:05:29 +0700

    add Auth by Handsome

---
+++ public/js/staff_dashboard.js
@@ -0,0 +1,23 @@
+// get user
+async function getUser() {
+    try {
+        const response = await fetch('/userInfo');
+        if (response.ok) {
+            const data = await response.json();
+            document.querySelector('#useremail').textContent = data.useremail;
+        }
+        else if (response.status == 500) {
+            const data = await response.text();
+            throw Error(data);
+        }
+        else {
+            throw Error('Connection error');
+        }
+    } catch (err) {
+        console.error(err.message);
+        Notiflix.Report.failure('Error', err.message, 'Close');
+    }
+}
+
+// get user info
+getUser();
